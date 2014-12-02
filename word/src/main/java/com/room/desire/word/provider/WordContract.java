33cfbfa10c8074742152b3968f73d0df698b0900
desire <room.desire@gmail.com>
2014-12-02 18:04:50 +0800

    Init word project.

---
+++ word/src/main/java/com/room/desire/word/provider/WordContract.java
@@ -0,0 +1,15 @@
+package com.room.desire.word.provider;
+
+/**
+ * Created by desire on 14/12/2.
+ */
+public interface WordContract {
+
+    public interface List {
+
+    }
+
+    public interface Word {
+
+    }
+}
